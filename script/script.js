155a19dc0243f1a95dd2a09c44347ffb5d783ca1
Nigel <nigel.ong00@gmail.com>
2022-11-08 10:55:07 -0800

    did commit and added object

--- script/script.js
+++ script/script.js
@@ -12,14 +12,52 @@
 //     console.log(i);
 //     i = i*2};
 
-let input = "";
-do{
-    input = prompt("What is your name?")
-    //prompt always returns a string
+// let input = "";
+// do{
+//     input = prompt("What is your name?")
+//     //prompt always returns a string
     
 
-    } while (
-        //!isNaN(input)
-        Number(input) || input == 0
-    )
-console.log(input);
+//     } while (
+//         (!isNaN(Number(input)))
+//         //Number(input) || input == 0
+//     )
+// console.log(input);
+
+// //alert("Welcome" + input);
+// alert(`Welcome ${input}`);
+
+// let fruits = ['Apple', 'Banana'];
+
+// fruits.push("Orange");
+// console.log(fruits);
+
+// fruits.pop();
+// console.log(fruits);
+
+// fruits.unshift("Mango");
+// console.log(fruits);
+
+// fruits.shift();
+// console.log(fruits);
+
+// for (let 1 = 0; i < fruits.length ; i++){
+//     console.long(fruits[i])
+// }
+            //better for finding index
+
+// for (let item of fruits){
+//     console.long(item)
+// }
+            // they do the same
+
+
+
+const person ={
+    name:["Bob", "Smith"],
+    age: 32,
+    gender:"male",
+    interest: ["music, skiing"],
+
+}
+console.log(`My name is ${person.name[0]} ${person.name[0]}. I am interested in${person.interest[0]} and ${person.interest[1]}.`)
